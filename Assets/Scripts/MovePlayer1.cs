0943e319275dcd040488aeb05c61bda78cb163df
AndreiTVR <andreiteianu2000@gmail.com>
2024-10-14 01:05:01 +0300

    continue

--- Assets/Scripts/MovePlayer1.cs
+++ Assets/Scripts/MovePlayer1.cs
@@ -3,29 +3,46 @@ using UnityEngine;
 
 public class MovePlayer1 : MonoBehaviour
 {
-    private float horizontal;
-    private float speed = 8f;
+    private float horizontal1;
+    private float horizontal2;
+    public float currentSpeed = 8f;
+    private float currentSpeed2 = 8f;
+    public float speed = 8f;
     private float jumpingPower = 10f;
     private bool isFacingRight = true;
-
+    private bool isFacingRight2 = false;
+    [SerializeField] private Collider2D crouchCollider;
     [SerializeField] private Rigidbody2D rb;
     [SerializeField] private Transform groundCheck;
     [SerializeField] private LayerMask groundLayer;
 
     void Update()
     {
-       if(IsGrounded()) horizontal = Input.GetAxisRaw("Horizontal1");
-
+        if (IsGrounded())
+        {
+            horizontal1 = Input.GetAxisRaw("Horizontal1");
+            
+        }
         if (Input.GetButtonDown("Jump1") && IsGrounded())
         {
             rb.velocity = new Vector2(rb.velocity.x, jumpingPower);
         }
+        if (Input.GetButtonDown("Crouch1") && IsGrounded())
+        {
+            currentSpeed = 0;
+            crouchCollider.enabled = false;
+        }
+        else if (Input.GetButtonUp("Crouch1"))
+        {
+            currentSpeed =speed;
+            crouchCollider.enabled = true;
+        }    
         Flip();
     }
 
     private void FixedUpdate()
     {
-        rb.velocity = new Vector2(horizontal * speed, rb.velocity.y);
+       rb.velocity = new Vector2(horizontal1 * currentSpeed, rb.velocity.y);
     }
 
     private bool IsGrounded()
@@ -35,7 +52,7 @@ public class MovePlayer1 : MonoBehaviour
 
     private void Flip()
     {
-        if (isFacingRight && horizontal < 0f || !isFacingRight && horizontal > 0f)
+        if (isFacingRight && horizontal1 < 0f || !isFacingRight && horizontal1 > 0f)
         {
             isFacingRight = !isFacingRight;
             Vector3 localScale = transform.localScale;
