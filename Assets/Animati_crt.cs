1d4388ee7c226e6f60ae50909e2efda95d0ecfe8
TheBlackMan2004 <alexandrulNegru@yahoo.com>
2024-10-16 00:05:33 +0300

    Stabilizat baza
    Teianu nu are drepturi

---
+++ Assets/Animati_crt.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animati_crt : MonoBehaviour
+{
+    Animator animator;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        bool merge = animator.GetBool("Merge");
+        bool mersFata = Input.GetKey("d");
+        bool mersSpate = Input.GetKey("a");
+        if (!merge && mersFata)
+        {
+            animator.SetBool("Merge", true);
+        }
+
+        if (merge && !mersFata)
+        {
+            animator.SetBool("Merge", false);
+        }
+
+        if (!merge && mersSpate)
+        {
+            animator.SetBool("Merge", true);
+        }
+
+        if (merge && !mersSpate)
+        {
+            animator.SetBool("Merge", false);
+        }
+    }
+}
